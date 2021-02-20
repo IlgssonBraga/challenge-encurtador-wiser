c151ce2bf3316b9c365050e2d81a76272a0614bf
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2021-02-20 16:04:19 -0300

    feat: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShortenerModule } from './shortener/shortener.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ShortenerModule],
+  imports: [ShortenerModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
